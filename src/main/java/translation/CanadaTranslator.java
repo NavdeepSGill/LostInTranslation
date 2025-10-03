00f01efc514dee39089502351397e9e3ef427e82
Dohyeon <dohyeon.nam@mail.utoronto.ca>
2025-10-02 22:43:18 -0400

    ori

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -48,7 +48,7 @@ public class CanadaTranslator implements Translator {
             return "Kanada";
         }
         else if (languageCode.equals("en")) {
-            return "Canadaaaa";
+            return "Canada";
         }
         else if ("zh".equals(languageCode)) {
             return "加拿大";
